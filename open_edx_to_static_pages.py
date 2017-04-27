39d84a41b58a48379410d91fabfcaada9ea6450d
jonnyBohnanny <jgrindstaff7@gatech.edu>
2017-04-26 19:40:27 -0500

    indent

--- open_edx_to_static_pages.py
+++ open_edx_to_static_pages.py
@@ -4,8 +4,7 @@ from ocx_to_html import CourseConverter
 
 if __name__ == "__main__":
     if len(sys.argv) > 0:
-
-    base = sys.argv[1]
-    course = CourseConverter.CourseConverter(base)
-    course.convert()
+        base = sys.argv[1]
+        course = CourseConverter.CourseConverter(base)
+        course.convert()
 
